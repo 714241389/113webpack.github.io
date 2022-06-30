76643c139653fe1ef65611e641be63f50f9792cb
HotFire <3466609300@qq.com>
2022-06-30 13:39:47 +0800

    update

---
+++ assets/index.3fcd7a1c.js
@@ -0,0 +1,2 @@
+import{q as o,i as n,o as i}from"./index.0b0797f8.js";const u=e=>{let t;return o(()=>{t=document.title}),n(()=>{document.title=e.title}),i(()=>{typeof t!="undefined"&&(document.title=t)}),[]};export{u as T};
+//# sourceMappingURL=index.3fcd7a1c.js.map
